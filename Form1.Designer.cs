44a021d8b866bfd08a9449c26cd7fb46fcc41325
Datorium <hello@datorium.lv>
2020-10-19 18:30:25 +0300

    version 18:30

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,13 +31,17 @@
             this.components = new System.ComponentModel.Container();
             this.ClockDisplay = new System.Windows.Forms.Label();
             this.ClockTimer = new System.Windows.Forms.Timer(this.components);
+            this.StartButton = new System.Windows.Forms.Button();
+            this.PauseButton = new System.Windows.Forms.Button();
+            this.ResetButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // ClockDisplay
             // 
             this.ClockDisplay.AutoSize = true;
             this.ClockDisplay.Font = new System.Drawing.Font("Montserrat", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ClockDisplay.Location = new System.Drawing.Point(57, 51);
+            this.ClockDisplay.ForeColor = System.Drawing.Color.White;
+            this.ClockDisplay.Location = new System.Drawing.Point(63, 36);
             this.ClockDisplay.Name = "ClockDisplay";
             this.ClockDisplay.Size = new System.Drawing.Size(469, 111);
             this.ClockDisplay.TabIndex = 0;
@@ -47,11 +51,54 @@
             // 
             this.ClockTimer.Tick += new System.EventHandler(this.ClockTimer_Tick);
             // 
+            // StartButton
+            // 
+            this.StartButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.StartButton.Font = new System.Drawing.Font("Montserrat", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StartButton.ForeColor = System.Drawing.Color.White;
+            this.StartButton.Location = new System.Drawing.Point(32, 167);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(169, 63);
+            this.StartButton.TabIndex = 1;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // PauseButton
+            // 
+            this.PauseButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.PauseButton.Font = new System.Drawing.Font("Montserrat", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PauseButton.ForeColor = System.Drawing.Color.White;
+            this.PauseButton.Location = new System.Drawing.Point(217, 167);
+            this.PauseButton.Name = "PauseButton";
+            this.PauseButton.Size = new System.Drawing.Size(169, 63);
+            this.PauseButton.TabIndex = 2;
+            this.PauseButton.Text = "Pause";
+            this.PauseButton.UseVisualStyleBackColor = true;
+            this.PauseButton.Click += new System.EventHandler(this.PauseButton_Click);
+            // 
+            // ResetButton
+            // 
+            this.ResetButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ResetButton.Font = new System.Drawing.Font("Montserrat", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ResetButton.ForeColor = System.Drawing.Color.White;
+            this.ResetButton.Location = new System.Drawing.Point(403, 167);
+            this.ResetButton.Name = "ResetButton";
+            this.ResetButton.Size = new System.Drawing.Size(169, 63);
+            this.ResetButton.TabIndex = 3;
+            this.ResetButton.Text = "Reset";
+            this.ResetButton.UseVisualStyleBackColor = true;
+            this.ResetButton.Click += new System.EventHandler(this.ResetButton_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(581, 296);
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(601, 274);
+            this.Controls.Add(this.ResetButton);
+            this.Controls.Add(this.PauseButton);
+            this.Controls.Add(this.StartButton);
             this.Controls.Add(this.ClockDisplay);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -64,6 +111,9 @@
 
         private System.Windows.Forms.Label ClockDisplay;
         private System.Windows.Forms.Timer ClockTimer;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button PauseButton;
+        private System.Windows.Forms.Button ResetButton;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -17,10 +17,11 @@ namespace DigitalColorClock
         int min = 0;
         int hou = 0;
 
+        Random rand = new Random();
+
         public Form1()
         {
-            InitializeComponent();
-            ClockTimer.Start();
+            InitializeComponent();            
         }
 
         private void ClockTimer_Tick(object sender, EventArgs e)
@@ -30,7 +31,22 @@ namespace DigitalColorClock
             {
                 dsec = 0;
                 sec += 1;
+                ChangeBackgroundColor();
             }
+            if(sec == 60)
+            {
+                sec = 0;
+                min += 1;
+            }
+            if(min == 60)
+            {
+                min = 0;
+                hou += 1;
+            }
+            if(hou == 24)
+            {
+                hou = 0;
+            }            
             UpdateClockDisplay();
         }
 
@@ -42,6 +58,37 @@ namespace DigitalColorClock
             timeText += sec.ToString("00") + ":";
             timeText += dsec.ToString("0");
             ClockDisplay.Text = timeText;
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            ClockTimer.Start();
+            StartButton.Text = "Start";
+        }
+
+        private void PauseButton_Click(object sender, EventArgs e)
+        {
+            ClockTimer.Stop();
+            StartButton.Text = "Resume";
+        }
+
+        private void ResetButton_Click(object sender, EventArgs e)
+        {
+            ClockTimer.Stop();
+            dsec = 0;
+            sec = 0;
+            min = 0;
+            hou = 0;
+            UpdateClockDisplay();
+            StartButton.Text = "Start";
+        }
+
+        private void ChangeBackgroundColor()
+        {
+            int redValue = rand.Next(0, 256);
+            int greenValue = rand.Next(0, 256);
+            int blueValue = rand.Next(0, 256);
+            this.BackColor = Color.FromArgb(redValue, greenValue, blueValue);
         }
     }
 }
